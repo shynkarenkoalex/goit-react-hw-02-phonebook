daaebd933b2a071d448ec9cd847a8a1d611bc275
Alex Shynkarenko <shynkarenkoalex5@gmail.com>
2020-03-15 19:42:34 +0200

    Updates

---
+++ precache-manifest.208c39e4b92b939bd8645ecf805758c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "818d5af4dfc328c3b765ad8fe600411d",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "6c9652cac275ad89be56",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.3c7d80c0.chunk.css"
+  },
+  {
+    "revision": "f741d88903de67249269",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.71c7605f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.71c7605f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c9652cac275ad89be56",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.21f3fdc0.chunk.js"
+  },
+  {
+    "revision": "5eea2426daa3643f03a9",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.6819ab10.js"
+  }
+]);
